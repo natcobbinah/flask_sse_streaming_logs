f0e8767a7bca32af6c779a81c62569d0faccfc9c
nathaniel cobbinah <natcobbinah1778@gmail.com>
2026-01-21 13:51:39 +0100

    Refactor log function to accept more parameters
    Enhance logging function to include additional context.

--- flask_sse_with_logs/utils/logger.py
+++ flask_sse_with_logs/utils/logger.py
@@ -1,5 +1,6 @@
 from enum import Enum 
 import logging 
+from typing import Optional, List, Union, Dict
 
 logger = logging.getLogger('flask_sse_with_logs_logger')
 
@@ -12,17 +13,43 @@ class LogLevel(Enum):
     EXCEPTION = "EXCEPTION"
     
 
+def log(*,
+        type: LogLevel,
+        component: Optional[str] = None,
+        event: Optional[str] = None,
+        filename: Optional[str] = None,
+        function_name: Optional[str] = None,
+        fileline: Optional[int] = None,
+        correlation_id: Optional[str] = None,
+        message: Optional[str] = None,
+        message_data: Optional[Union[Dict, List, str, set, tuple]] = None,
+        reason: Optional[str] = None,
+       ) -> None:
+    
+    # Prepare the extra context for logging
+    extra_context = {
+        'component': component,
+        'event': event,
+        'appfilename': filename,
+        'function_name': function_name,
+        'fileline': fileline,
+        'correlation_id': correlation_id,
+        'reason': reason,
+        'message_data': message_data
+    }
 
-def log(*, type=LogLevel, message:any) -> None:
+    # Log the message based on the log level
+    # The `extra` parameter is used to pass additional context to the logger. 
+    # This ensures that the additional fields (`component`, `event`, etc.) are included in the log record.
     if type == LogLevel.DEBUG:
-        logger.debug(message)
+        logger.debug(message, extra=extra_context)
     elif type == LogLevel.INFO:
-        logger.info(message)
+        logger.info(message, extra=extra_context)
     elif type == LogLevel.WARNING:
-        logger.warning(message)
+        logger.warning(message, extra=extra_context)
     elif type == LogLevel.ERROR:
-        logger.error(message)
+        logger.error(message, extra=extra_context)
     elif type == LogLevel.CRITICAL:
-        logger.critical(message)
+        logger.critical(message, extra=extra_context)
     elif type == LogLevel.EXCEPTION:
-        logger.exception(message)
+        logger.exception(message, extra=extra_context)
